b9ad73f4e46c3ce85dbfe0f683260a9fe3587137
anttross <anttross@gmail.com>
2015-08-16 19:07:20 +0300

    fixed db

---
+++ Event/Event/DAL/EventContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Event.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Event.DAL
+{
+    public class EventContext : DbContext
+    {
+        public EventContext()
+            : base("DefaultConnection")
+        {
+        }
+        
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Events> Events { get; set; }
+      
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
---
+++ Event/Event/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Event.Models
+{
+    public class Events
+    {
+        public int ID { get; set; }
+       // public int UserID { get; set; }
+        public string Event { get; set; }
+        public DateTime Date { get; set; }
+
+       
+
+    }
+}
